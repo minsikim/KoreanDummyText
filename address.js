892631c9d2e2f37bac6b9c0d2f7f5c9d691b347b
minsikim <minsiua@gmail.com>
2018-02-01 11:41:50 +0900

    address function

---
+++ address.js
@@ -0,0 +1,9 @@
+// var a = 'http://localhost/';
+var a = 'C:\\Users\\MinSKim\\Documents\\GitHub\\';
+
+function test(){
+  document.write("Hello");
+}
+function linkCSS(dir) {
+    document.write("<link rel=\"stylesheet\" href=\""+a+dir"\"/>");
+}
