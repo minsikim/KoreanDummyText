cccdaa88c7befd3a1e5bbacaed61a6b2d3bb648e
minsikim <minsiua@gmail.com>
2018-02-04 04:39:08 +0900

    guess language function

--- gen.js
+++ gen.js
@@ -7,9 +7,28 @@
 
 function generate() {
   var gt = document.getElementById('gentext');
+  //gt.innerHTML.clear();
   gt.innerHTML = "";
   var input = document.getElementById('gen2').value;
-  console.log(input);
+
+
+
+  function guessLang (textin) {
+    guessLanguage.detect(textin, function(language) {
+    console.log('Detected language code of provided text is [' + language + ']');
+    return language;
+    });
+  }
+
+    var lang = guessLang(input);
+
+  console.log(lang);
+  if(lang !== 'eng') {
+    alert('We do not support languages other than English.')
+    return;
+  }
+
+  // FileReader.readAsText(ref.txt);
   gt.innerHTML += input;
 
 }
