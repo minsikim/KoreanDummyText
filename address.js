419f3df9d3bf904cfcbf0ab5d6a527e4e57b917b
minsikim <minsiua@gmail.com>
2018-02-06 03:18:30 +0900

    updating readable json

--- address.js
+++ address.js
@@ -1,8 +1,50 @@
 // var a = 'http://localhost/';
+var curr = 'local';
+
 var address = {
-  'local' : 'C:\\Users\\MinSKim\\Documents\\GitHub\\',
-  'salinger' : 'http://salinger.ms/'
+  localhost : 'http://localhost/',
+  local : 'C:\\Users\\MinSKim\\Documents\\GitHub\\',
+  localProject : 'C:\\Users\\MinSKim\\Documents\\GitHub\\KoreanDummyText\\',
+  salinger : 'http://salinger.me/',
+
+  get : function() {
+    // for checking arguments
+
+    // for(var args in arguments){
+    //     console.log(arguments[args]);
+    //     console.log('is type : ' + typeof arguments[args]);
+    // }
+
+    if(!arguments){
+      return 'no arguments found in _get function';
+    }
+    if(arguments.length === 3){
+      var dir = arguments[0];
+      var filename = arguments[1];
+      var callback = arguments[2];
+      if(this[dir]){
+        return callback(this[dir]+filename);
+      }
+      return 'no matching directory found'
+    }
+    if(arguments.length === 2){
+      var dir = arguments[0];
+      var filename = arguments[1];
+        if(this[dir]){
+          return this[dir]+filename;
+        }
+        return 'no matching directory found'
+    }
+    if(arguments.length === 1){
+      var dir = arguments[0];
+      if(this[dir]){
+        return this[dir];
+      }
+      return 'no matching directory found'
+    }
+  }
 }
+
 // function test(){
 //   document.write("Hello");
 // }
--- gen.js
+++ gen.js
@@ -4,6 +4,21 @@
 // function preload(){
 //   txt = loadStrings("ref.txt")
 // }
+var arr = JSON.parse(ref);
+
+// function LoadFile (){
+//   var oFrame = document.getElementById("frmFile");
+//   var strRawContents = oFrame.contentWindow.document.body.childNodes[0].innerHTML;
+//   while(strRawContents.indexOf("\r") >= 0){
+//     strRawContents = strRawContents.replace("\r", "");
+//   }
+//   var arrLines = strRawContents.split("\n");
+//   alert("File " + oFrame.src + " has " + arrLines.length + " lines");
+//   for (var i = 0; i < arrLines.length; i++) {
+//     var curLine = arrLines[i];
+//     alert("Line #" + (i + 1) + " is: '" + curLine + "'");
+//   }
+// }();
 
 function generate() {
   var gt = document.getElementById('gentext');
