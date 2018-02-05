f47ebb7e0c7526fe334973234ea619c66034e748
minsikim <minsiua@gmail.com>
2018-02-05 22:34:25 +0900

    changes to lib

--- gen.js
+++ gen.js
@@ -10,26 +10,28 @@ function generate() {
   //gt.innerHTML.clear();
   gt.innerHTML = "";
   var input = document.getElementById('gen2').value;
-
-  var lang = "";
-
-
-    guessLanguage.detect(input, function(language) {
-    console.log('Detected language code of provided text is [' + language + ']');
-    console.log(typeof language);
-    this.lang = language;
-    console.log(typeof lang);
-    console.log(lang);
-    });
-
-
+  var lang = guessLanguage.detect(input, function(language) {
+  console.log('Detected language code of provided text is [' + language + ']');
+  return language;
+  });
+  console.log(typeof lang);
   console.log(lang);
-  if(lang !== 'eng') {
+
+  if(lang !== 'ko') {
     alert('We do not support languages other than English.')
     return;
   }
 
-  // FileReader.readAsText(ref.txt);
+
+
   gt.innerHTML += input;
 
 }
+
+// function getStringfromtxt(){
+//   var result = [];
+//   for(var fileLocation in arguments){
+//
+//   }
+//   return result;
+// }
