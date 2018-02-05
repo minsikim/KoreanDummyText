ba395acfcd182edd0f6a05a0d938c3ee43dc9da5
minsikim <minsiua@gmail.com>
2018-02-06 04:54:36 +0900

    main js modified
    primary structure complete

--- gen.js
+++ gen.js
@@ -20,6 +20,10 @@ var arr = JSON.parse(ref);
 //   }
 // }();
 
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 function generate() {
   var gt = document.getElementById('gentext');
   //gt.innerHTML.clear();
@@ -29,17 +33,49 @@ function generate() {
   console.log('Detected language code of provided text is [' + language + ']');
   return language;
   });
-  console.log(typeof lang);
-  console.log(lang);
+  // console.log(typeof lang);
+  // console.log(lang);
 
   if(lang !== 'ko') {
     alert('We do not support languages other than English.')
     return;
   }
+  var inputArr = input.split('');
+  console.log("inputArr : "+inputArr);
+  var result = [];
 
+  while(result.length < 100){
+    var temp = ref[getRandomInt(0,ref.length)];
+    if(temp){
+      var tempArr = temp.split('');
+      var match = true;
 
+      for(var index in tempArr){
+        if(!inputArr.includes(tempArr[index])){
+          match = false;
+        }
+      }
+      if(match === true && !result.includes(temp)){
+        result.push(temp);
+      }
+    }
+  }
+  var count = 0;
+  console.log(result + typeof result);
+  while(count < 500){
+    var s = result[getRandomInt(0,result.length)];
+    // console.log(s + typeof s);
+    if(s.charAt(s.length-1) !== '다'){
+      gt.innerHTML += s+' ';
+      count++;
+    }
+    else{
+      gt.innerHTML += s+'. ';
+      count++;
+    }
 
-  gt.innerHTML += input;
+  }
+
 
 }
 
